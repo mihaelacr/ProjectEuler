f2f6f0524775201c9b8beac7775997e919d364c4
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-19 11:16:08 +0000

    Added comments with links with the problems from the site

--- Problem5.hs
+++ Problem5.hs
@@ -1,3 +1,5 @@
+-- http://projecteuler.net/problem=5
+
 -- 2 5
 -- 3 2
 -- 5 1
--- Problem6.hs
+++ Problem6.hs
@@ -1,2 +1,4 @@
+-- http://projecteuler.net/problem=6
+
 f :: Int -> Int
 f n = ((n * (n + 1)) `div` 2 ) ^ 2 - ((n * (n + 1) * (2 * n + 1)) `div` 6)

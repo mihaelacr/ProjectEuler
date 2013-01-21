679477dba3cb620dcb89a2789395cc22fa1c0b4a
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-21 00:19:21 +0000

    Comments

--- Problem12.hs
+++ Problem12.hs
@@ -1,16 +1,20 @@
+-- http://projecteuler.net/problem=12
 import Sieve
 
 limit:: Int
 limit = 500
 
-
+-- Computes the number of divisors of a number
 nrDivs :: Int -> Int
 nrDivs n = 2 * (1 + length [i | i<- [2 .. floor $ sqrt $ fromIntegral n ], n `mod` i == 0])
 
-
+-- Computes the n'th triangular number
 triangular :: Int -> Int
 triangular n = n * (n + 1) `div` 2
 
+-- Computes the number of divisors of a triangular number
+-- by taking into account that gcd(n, n+1) = 1
+nrDivTriangular :: Int -> Int
 nrDivTriangular n
   | odd n = nrDivs n * (nrDivs ((n + 1) `div` 2))
   | otherwise = nrDivs (n `div` 2) * (nrDivs (n + 1))
@@ -18,7 +22,6 @@ nrDivTriangular n
 
 res :: Int
 res = triangular $ head $ dropWhile (\x -> nrDivTriangular x < limit) [1 ..]
-
 
 -- Computes the number of divisors using Euler's function, but it is more
 -- expensice than simple version nrDivs (above)

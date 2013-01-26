7319eb4c585035498e9df82bbf4edd302d68e1f7
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-26 11:19:24 +0000

    Comments and clean up

--- Problem14.hs
+++ Problem14.hs
@@ -15,20 +15,18 @@ f x
   | otherwise = 3 * x + 1
 
 
-getInfiniteSequence :: Int -> [Int]
-getInfiniteSequence = iterate f
-
-
 -- Gets the Collatz sequence (without the 1, but this is uneeded for comparison)
 getSequence :: Int -> [Int]
 getSequence 1 = []
-getSequence n = n: takeWhile (/= 1) (getInfiniteSequence n)
+getSequence n = n: takeWhile (/= 1) (iterate f n)
 
 
-getSequence1 :: Int -> Storage -> Storage
-getSequence1 n seqs
+-- Computes the length Collatz sequence of n by using the already computes values given in seqs
+getSequenceLength :: Int -> Storage -> Storage
+getSequenceLength n seqs
   | even n = M.insert n  (1 + fromJust (M.lookup (f n) seqs))  seqs
-  | otherwise = let (partial, rest) = span (>= n) $ getSequence n
+  | otherwise = let (partial, rest) = span (>= n) $ getSequence n -- lazy evaluation at its best
+                                                                  -- rest never gets computed, apart from it's head
                     additional = length partial
                     newLengths =  [additional , -1 .. 1]
                 in case rest of
@@ -36,20 +34,22 @@ getSequence1 n seqs
                   (r : _) -> M.fromList (zip partial $ Prelude.map (+ fromJust (M.lookup r seqs)) newLengths) `M.union` seqs
 
 
-m1 :: Int -> Storage -> Storage
-m1 n seqs
+-- Computes all the sequence lengths by storing what has been computed already
+getAllSequenceLengths :: Int -> Storage -> Storage
+getAllSequenceLengths n seqs
   | n >= limit = seqs
-  | otherwise = m1 (n + 1) (getSequence1 n seqs)
+  | otherwise = getAllSequenceLengths (n + 1) (getSequenceLength n seqs)
 
 
-sndOrd :: Ord b => (a, b) -> (a, b) -> Ordering
-sndOrd (_, y1) (_, y2) = compare y1 y2
+maximumBySecond :: Ord b => [(a, b)] -> (a, b)
+maximumBySecond = maximumBy sndOrd
+  where sndOrd (_, y1) (_, y2) = compare y1 y2
 
 maxSeq :: (Int, Int)
-maxSeq = maximumBy sndOrd $ M.toList $ m1 1 M.empty
+maxSeq = maximumBySecond $ M.toList $ getAllSequenceLengths 1 M.empty
 
 maxSeqBruteForce :: (Int, Int)
-maxSeqBruteForce = maximumBy sndOrd [(i, length $ getSequence i) | i <- [1 .. limit + 1]]
+maxSeqBruteForce = maximumBySecond [(i, length $ getSequence i) | i <- [1 .. limit + 1]]
 
 main :: IO()
-main = print maxSeq
+main = print maxSeqBruteForce

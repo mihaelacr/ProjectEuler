4ad1328a8ed3f45c9983c8300980254221cf5349
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-29 22:12:38 +0000

    Replaced single sum list with version which computes all the partial sums but this implementation is not correct yet

--- Problem67.hs
+++ Problem67.hs
@@ -8,21 +8,23 @@ filename = "Problem67.txt"
 filename1 :: String
 filename1 = "sample.txt"
 
- -- replace s1 with something like this
--- maximum getMaxSum ( [[l !! i !! j | j <- [i .. length l !! i]] |  i <- [ 2 .. length l])
+-- TODO replace with one function and one
+getLeftTriangleAt :: [[Int]] -> Int -> [[Int]]
+getLeftTriangleAt xs index = [ take (i - index + 1)  $ xs !! i |  i <- [index .. length xs - 1]]
+
+getRightTriangleAt :: [[Int]] -> Int -> [[Int]]
+getRightTriangleAt xs index = [ drop (i - 1)  $ xs !! i |  i <- [index .. length xs - 1]]
 
 
 -- precondition:: the lengths of the lists are consecutive numvers
 getMaxSum :: [[Int]] -> Int
 getMaxSum [] = 0
 getMaxSum [x] = head x
-getMaxSum (x:y:xs) = head x + max s1 (getMaxSum rest)
+getMaxSum l@(x:y:xs) = head x + max s1 (getMaxSum rest)
   where y1:y2: _ = y
-      -- it is not as easy as finding out s1, but the maximum of
-      -- all triangles which start in the left most or right most corner of the respectively
-      -- chosen edge
-        s1 = if y1 > y2  then sum $ map last (y:xs)
-                         else sum $ map head (y:xs)
+        size = length l
+        s1 = if y1 > y2  then maximum $ map (getMaxSum . getLeftTriangleAt l)  [2.. size - 1]
+                         else maximum $ map (getMaxSum . getRightTriangleAt l) [2.. size - 1]
 
         rest = if y1 > y2 then map init (y:xs)
                           else map tail (y:xs)

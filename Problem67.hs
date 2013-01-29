5ca020a5263b9bad6a755fad1e62fb067d9b0f84
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-29 09:43:29 +0000

    Found bug and wrote commnets on how to fix it

--- Problem67.hs
+++ Problem67.hs
@@ -2,26 +2,35 @@
 -- this solution also works for problem 18
 
 
--- naive solution for problem 18 would not work in this case
-
 filename :: String
 filename = "Problem67.txt"
+
+filename1 :: String
+filename1 = "sample.txt"
+
+ -- replace s1 with something like this
+-- maximum getMaxSum ( [[l !! i !! j | j <- [i .. length l !! i]] |  i <- [ 2 .. length l])
 
 
 -- precondition:: the lengths of the lists are consecutive numvers
 getMaxSum :: [[Int]] -> Int
 getMaxSum [] = 0
 getMaxSum [x] = head x
-getMaxSum l@(x:y:xs) = max s1 (head x + getMaxSum rest)
+getMaxSum (x:y:xs) = head x + max s1 (getMaxSum rest)
   where y1:y2: _ = y
-        s1 = if y1 > y2 then sum $ map head l
-                        else sum $ map last l
-        rest = if y1 > y2 then map tail (y:xs)
-                          else map init (y:xs)
-sol :: IO()
-sol = do
-  triangle <- readFile filename
+      -- it is not as easy as finding out s1, but the maximum of
+      -- all triangles which start in the left most or right most corner of the respectively
+      -- chosen edge
+        s1 = if y1 > y2  then sum $ map last (y:xs)
+                         else sum $ map head (y:xs)
+
+        rest = if y1 > y2 then map init (y:xs)
+                          else map tail (y:xs)
+
+sol :: String -> IO()
+sol file = do
+  triangle <- readFile file
   let values = (map (map read . words) $ lines triangle) :: [[Int]]
-  -- print $ map length values
+  -- print values
   print $ getMaxSum values
 

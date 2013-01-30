025662028ad2c9c3c86752ec7b3832f7a9a0c1b3
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-30 17:03:41 +0000

    Rewwrote everything with vector and fixed it such that it works for Problem18. Still not good enough for 67, as it is too slow

--- Problem67.hs
+++ Problem67.hs
@@ -1,6 +1,7 @@
 -- http://projecteuler.net/problem=67
 -- this solution also works for problem 18
 
+import qualified Data.Vector as V
 
 filename :: String
 filename = "Problem67.txt"
@@ -8,30 +9,43 @@ filename = "Problem67.txt"
 filename1 :: String
 filename1 = "sample.txt"
 
+filename18 :: String
+filename18 = "Problem18.txt"
+
+
 data TriangleType = LeftT | RightT
 
+type Triangle = V.Vector (V.Vector Int)
 
-getTriangleAt :: TriangleType -> [[Int]] -> Int -> [[Int]]
-getTriangleAt LeftT xs index  = [ take (i - index + 1) $ xs !! i |  i <- [index .. length xs - 1]]
-getTriangleAt RightT xs index = [ drop index  $ xs !! i |  i <- [index .. length xs - 1]]
+-- | Converts a list of lists to a 2D vector
+vector2D :: [[a]] -> V.Vector (V.Vector a)
+vector2D ll = V.fromList $ map V.fromList ll
 
 
--- precondition:: the lengths of the lists are consecutive numvers
-getMaxSum :: [[Int]] -> Int
-getMaxSum [] = 0
-getMaxSum [x] = head x
-getMaxSum l@(x:y:xs) = if null xs then head x + max y1 y2
-                                  else head x + max s1 (getMaxSum rest)
-  where y1:y2: _ = y
-        s1 = if y1 > y2  then getMaxSum $ getTriangleAt LeftT l 2
-                         else getMaxSum $ getTriangleAt RightT l 2
-        rest = if y1 > y2 then map init (y:xs)
-                          else map tail (y:xs)
+getTriangleAt :: TriangleType -> Triangle -> Int -> Triangle
+getTriangleAt LeftT xs index  = V.fromList [ V.take (i - index + 1) $ xs V.! i |  i <- [index .. V.length xs - 1]]
+getTriangleAt RightT xs index = V.fromList [ V.drop index $ xs V.! i |  i <- [index .. V.length xs - 1]]
+
+
+getMaxSum :: Triangle -> Int
+getMaxSum t = case V.length t of
+  0 -> 0
+  1 -> V.head $ V.head t
+  _ -> if V.null xs then x + max y1 y2
+                    else x + max s1 (getMaxSum rest)
+  where x  = V.head $ V.head t
+        y1 = V.head $ (V.!) t 1
+        y2 = (V.!) ((V.!) t 1) 1
+        xs = V.drop 2 t
+        s1 = if y1 > y2 then y2 + getMaxSum (getTriangleAt RightT t 2)
+                        else y1 + getMaxSum (getTriangleAt LeftT  t 2)
+        rest = if y1 > y2 then getTriangleAt LeftT t 1
+                          else getTriangleAt RightT t 1
+
 
 sol :: String -> IO()
 sol file = do
   triangle <- readFile file
   let values = (map (map read . words) $ lines triangle) :: [[Int]]
-  -- print values
-  print $ getMaxSum values
+  print $ getMaxSum $ vector2D values
 

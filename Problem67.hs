62fbe944ae14fad6dd8bb8376738c6ad869c8cea
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-30 13:46:38 +0000

    removed some useless stuff and refactored some function. Still takes ages.

--- Problem67.hs
+++ Problem67.hs
@@ -8,24 +8,23 @@ filename = "Problem67.txt"
 filename1 :: String
 filename1 = "sample.txt"
 
--- TODO replace with one function and one
-getLeftTriangleAt :: [[Int]] -> Int -> [[Int]]
-getLeftTriangleAt xs index = [ take (i - index + 1)  $ xs !! i |  i <- [index .. length xs - 1]]
+data TriangleType = LeftT | RightT
 
-getRightTriangleAt :: [[Int]] -> Int -> [[Int]]
-getRightTriangleAt xs index = [ drop (i - 1)  $ xs !! i |  i <- [index .. length xs - 1]]
+
+getTriangleAt :: TriangleType -> [[Int]] -> Int -> [[Int]]
+getTriangleAt LeftT xs index  = [ take (i - index + 1) $ xs !! i |  i <- [index .. length xs - 1]]
+getTriangleAt RightT xs index = [ drop index  $ xs !! i |  i <- [index .. length xs - 1]]
 
 
 -- precondition:: the lengths of the lists are consecutive numvers
 getMaxSum :: [[Int]] -> Int
 getMaxSum [] = 0
 getMaxSum [x] = head x
-getMaxSum l@(x:y:xs) = head x + max s1 (getMaxSum rest)
+getMaxSum l@(x:y:xs) = if null xs then head x + max y1 y2
+                                  else head x + max s1 (getMaxSum rest)
   where y1:y2: _ = y
-        size = length l
-        s1 = if y1 > y2  then maximum $ map (getMaxSum . getLeftTriangleAt l)  [2.. size - 1]
-                         else maximum $ map (getMaxSum . getRightTriangleAt l) [2.. size - 1]
-
+        s1 = if y1 > y2  then getMaxSum $ getTriangleAt LeftT l 2
+                         else getMaxSum $ getTriangleAt RightT l 2
         rest = if y1 > y2 then map init (y:xs)
                           else map tail (y:xs)
 

995084679a999e53a81dcb92de3b4e4c0137cdfb
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-01-31 10:15:43 +0000

    Added a third solution

--- Problem21.hs
+++ Problem21.hs
@@ -8,20 +8,24 @@ limit = 10000
 d :: Int -> Int
 d n = 1 + sum [i | i <- [2 .. n `div` 2], n `mod` i == 0]
 
+
 l1 :: [(Int, Int)]
 l1 = [(i , d_i) | i  <- [1 .. limit], let d_i = d i, d_i /= i]
-l2 :: [(Int, Int)]
-l2 = [(a, b) | (b, a) <- l1]:
 
+m1:: M.Map Int Int
 m1 = M.fromList l1
-m2 = M.fromList l2
 
 f :: M.Map Int Int -> Int -> Int -> Int -> Int
 f m k val s = if (M.lookup val m == Just k) && (k /= val) then s + k
                                                           else s
-sum1 :: Int
-sum1 = M.foldWithKey (f m1) 0 m1
+sol1 :: Int
+sol1 = M.foldWithKey (f m1) 0 m1
 
+l2 :: [(Int, Int)]
+l2 = [(a, b) | (b, a) <- l1]
 
-sm :: Int
-sm = sum $ map fst $ intersect l1 l2
+sol2 :: Int
+sol2 = sum $ map fst $ intersect l1 l2
+
+sol3 :: Int
+sol3 =  sum [x | (x, y) <- zip [1 .. limit] [d (d i) | i <- [ 1.. limit] ], x == y, d x /= x]

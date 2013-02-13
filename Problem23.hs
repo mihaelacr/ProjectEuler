2ccffed5496ee6b5e4190409d46348d7dcf94f40
Mihaela Rosca <mihaelacrosca@gmail.com>
2013-02-13 16:54:51 +0000

    Fixed Niklas version and added TODO for improvement

--- Problem23.hs
+++ Problem23.hs
@@ -2,6 +2,7 @@ import qualified Data.Set as S
 -- http://projecteuler.net/problem=23
 import Problem21
 
+-- TODO: Replace "any isAbundant" by set-lookup (O(log n)) or array lookup (O(1))
 
 start:: Int
 start = 12
@@ -16,21 +17,24 @@ isAbundant x = d x > x
 find :: [Int] -> [Int] -> Int -> Int
 find [] _ s = s
 find (x:xs) abund s
-  = if isAbundant x then find xs (x:abund) (s + add)
-                    else find xs abund (s + add)
-      where ms = any isAbundant [ x - n | n <- abund]
-            add = if ms then 0 else x
+ = if isAbundant x then find xs (x:abund) (s + add)
+                   else find xs abund (s + add)
+     where ms = any isAbundant [ x - n | n <- abund]
+           add = if ms then 0 else x
 
 res :: Int
 res = find [start .. stop] [] 0 + 66
 
 
 res' :: Int
-res' = sum unwritables
-  where
-    abundants = filter isAbundant [start .. stop]
-    unwritables = filter unwritable [start .. stop]
-    unwritable x = any isAbundant [ x - n | n <- takeWhile (< x) abundants ]
+res' = 66 + sum unwritables
+ where
+   l = [start .. stop]
+   abundants = filter isAbundant l
+   unwritables = filter (not . writable) l
+   writable x = any isAbundant [ x - n | n <- takeWhile (< x) abundants ]
 
 
-main = print res'
+-- main = mapM print (take 100 res')
+main = print res
+
